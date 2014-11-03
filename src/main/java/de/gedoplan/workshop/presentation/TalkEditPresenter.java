c0bd4b62b8612470110855255dd91d43234867db
Gabor <gabor.hajba@gmail.com>
2014-11-03 15:00:36 +0100

    separated editing presenters

---
+++ src/main/java/de/gedoplan/workshop/presentation/TalkEditPresenter.java
@@ -0,0 +1,39 @@
+package de.gedoplan.workshop.presentation;
+
+import javax.faces.flow.FlowScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import de.gedoplan.workshop.domain.Talk;
+import de.gedoplan.workshop.domain.TalkType;
+import de.gedoplan.workshop.persistence.TalkRepository;
+
+/**
+ * @author GHajba 2014 nov. 3
+ *
+ */
+@Named
+@FlowScoped(value = "talk")
+public class TalkEditPresenter {
+
+    @Inject
+    @Current
+    private Talk currentTalk;
+
+    @Inject
+    TalkRepository talkRepository;
+
+    public Talk getCurrentTalk() {
+        return this.currentTalk;
+    }
+
+    public String save() {
+        this.talkRepository.merge(this.currentTalk);
+        return "exit";
+    }
+
+    public TalkType[] getTalkTypes() {
+        return TalkType.values();
+    }
+
+}
--- src/main/java/de/gedoplan/workshop/presentation/TalkPresenter.java
+++ src/main/java/de/gedoplan/workshop/presentation/TalkPresenter.java
@@ -2,14 +2,13 @@ package de.gedoplan.workshop.presentation;
 
 import java.util.List;
 
+import javax.enterprise.inject.Produces;
 import javax.faces.flow.FlowScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import de.gedoplan.workshop.domain.Talk;
-import de.gedoplan.workshop.domain.TalkType;
 import de.gedoplan.workshop.persistence.All;
-import de.gedoplan.workshop.persistence.TalkRepository;
 
 /**
  * @author GHajba 2014 nov. 3
@@ -22,31 +21,17 @@ public class TalkPresenter {
     @Inject
     @All
     private List<Talk> talks;
-    private Talk currentTalk;
 
-    @Inject
-    TalkRepository talkRepository;
+    @Produces
+    @Current
+    private Talk currentTalk;
 
     public List<Talk> getTalks() {
         return this.talks;
-    }
-
-    public Talk getCurrentTalk() {
-        return this.currentTalk;
     }
 
     public String select(Talk talk) {
         this.currentTalk = talk;
         return "talkEdit.xhtml";
     }
-
-    public String save() {
-        this.talkRepository.merge(this.currentTalk);
-        return "exit";
-    }
-
-    public TalkType[] getTalkTypes() {
-        return TalkType.values();
-    }
-
 }
